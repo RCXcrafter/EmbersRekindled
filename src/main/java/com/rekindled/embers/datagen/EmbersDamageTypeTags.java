d29acb1e7810b49c61e9f0dad43664b027a0fb7f
RCXcrafter <rcxcrafter@gmail.com>
2023-12-30 10:28:39 +0100

    make ember damage consecrate undead with consecration installed

--- src/main/java/com/rekindled/embers/datagen/EmbersDamageTypeTags.java
+++ src/main/java/com/rekindled/embers/datagen/EmbersDamageTypeTags.java
@@ -8,11 +8,15 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.TagsProvider;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.DamageTypeTags;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.damagesource.DamageType;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
 public class EmbersDamageTypeTags extends TagsProvider<DamageType> {
+
+	public static final TagKey<DamageType> HOLY_DAMAGE = TagKey.create(Registries.DAMAGE_TYPE, new ResourceLocation("consecration", "holy"));
 
 	public EmbersDamageTypeTags(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, ExistingFileHelper existingFileHelper) {
 		super(output, Registries.DAMAGE_TYPE, lookupProvider, Embers.MODID, existingFileHelper);
@@ -22,6 +26,7 @@ public class EmbersDamageTypeTags extends TagsProvider<DamageType> {
 	protected void addTags(HolderLookup.Provider provider) {
 		tag(DamageTypeTags.BYPASSES_ARMOR).addOptional(EmbersDamageTypes.EMBER_KEY.location());
 		//tag(DamageTypeTags.IS_FIRE).addOptional(EmbersDamageTypes.EMBER_KEY.location());
+		tag(HOLY_DAMAGE).addOptional(EmbersDamageTypes.EMBER_KEY.location());
 		tag(DamageTypeTags.WITCH_RESISTANT_TO).addOptional(EmbersDamageTypes.EMBER_KEY.location());
 	}
 }
